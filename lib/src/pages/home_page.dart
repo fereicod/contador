36255e47e7f1bdc7acd776c3b4dfa22f9ef8ed6a
Fernando E.I <mferna.92@gmail.com>
2020-01-06 17:58:34 -0600

    add widget Column and style in text

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,16 +2,23 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   
+  final estiloTexto = TextStyle(fontSize: 25);
+
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: Text("Titulo"),
         centerTitle: true,
       ),
       body: Center(
-        child: Text("Numero de clicks"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Número de clicks', style: estiloTexto),
+            Text('0', style: estiloTexto)
+          ],
+        )
       ),
     );
   }
